07fada5f94775a2762ce0beb57acf07da7f8169f
Uncle10 <117525299+ThienLuong1101@users.noreply.github.com>
2023-08-14 14:08:50 +0930

    Add files via upload

---
+++ main-1-4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include"Person.h"
+using namespace std;
+
+extern PersonList createPersonList(int n);
+extern  PersonList deepCopyPersonList(PersonList pl);
+extern PersonList shallowCopyPersonList(PersonList pl);
+
+int main() {
+    int n;
+    cout << "enter a number: ";
+    cin >> n;
+    PersonList ans = createPersonList(n);
+    PersonList res = deepCopyPersonList(ans);
+    PersonList shallow = shallowCopyPersonList(res);
+    for (int i = 0; i < n; i++)
+    {
+        cout << shallow.people[i].name << " " << shallow.people[i].age << endl;
+    }
+
+    return 0;
+}
