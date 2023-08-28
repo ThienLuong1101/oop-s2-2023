2cd6f950d35ea06b6882e5e2e6c7549f5978b56d
Uncle10 <117525299+ThienLuong1101@users.noreply.github.com>
2023-08-28 10:41:11 +0930

    Add files via upload

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,35 +1,128 @@
-#include <iostream>
-#include <vector>
-#include"Bike.h"
-#include"Bike_yard.h"
+#include<iostream>
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include<string>
+using namespace std;
 
+class Vehicle {
+    private:
+        time_t timeOfEntry = time(nullptr);
+        int ID;
+    public:
+    Vehicle(){};
+    Vehicle( int id) : ID(id) {}
+    time_t getTime() {return timeOfEntry;}
+    int getID() {return ID;}
+    virtual int getParkingDuration() = 0;
+    ~Vehicle() {};
+};
+
+class Car : public Vehicle {
+    public:
+    Car(int id) : Vehicle(id) {}
+    int getParkingDuration(){ 
+        
+        time_t currTime = time(nullptr);
+        int elapsedTime = static_cast<int>(currTime - getTime());
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 - 900));
+        return elapsedTime; 
+    
+    }
+    
+};
+
+class Bus : public Vehicle {
+    public:
+    Bus(int id) : Vehicle(id) {}
+    int getParkingDuration() {
+    
+        time_t currTime = time(nullptr);
+        int elapsedTime = static_cast<int>(currTime - getTime());
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 - 750));
+        return elapsedTime; 
+    }
+};
+
+class Motobike : public Vehicle {
+    public:
+    Motobike(int id) : Vehicle(id){}
+    int getParkingDuration() {
+        
+        time_t currTime = time(nullptr);
+        int elapsedTime = static_cast<int>(currTime - getTime());
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 - 850));
+        return elapsedTime; 
+        
+    }
+};
+
+class ParkingLot {
+    private:
+        int max;
+        int curr = 0;
+    public:
+        ParkingLot(int cap) : max(cap) {}
+        Vehicle** park = new Vehicle*[max];
+        int getCount() {return curr;}
+        void parkVehiecle(Vehicle* a) {
+            if (curr < max)
+            {
+                park[curr] = a;
+                curr++;
+            }
+            else
+            {
+                cout << "The lot is full" << endl;
+            }  
+        } 
+        void unparkVehiecle(int id) {
+            int found = 0;
+            for (int i = 0; i < max; i++)
+            {
+                if (park[i]->getID() == id)
+                {
+                    delete park[i];
+                    found++;
+                    park[i] = nullptr;
+                }
+            }
+            if (!found)
+            {
+                cout<<"Vehicle not in the lot" << endl;
+            }
+            
+            
+        } 
+
+};
 int main() {
-    Bike_yard test(3);
+    int c, b, m;
+    Vehicle* ParkingLot[10];
+    string type;
+      
+    for (int i = 0; i < 10; i++)
+    {
+        cout<< "What type of vehicle parking here (car,bus,motobike): ";
+        cin >> type;
+        if (type == "car")
+        {
+            ParkingLot[i] = new Car(i);
+            ParkVehicle
+        }
+        else if (type == "bus")
+        {
+            ParkingLot[i] = new Bus(i);
+        }
+        else if (type == "motobike")
+        {
+            ParkingLot[i] = new Motobike(i);
+        }
+    }
     
-    Bike a("An",2004);
-    Bike b("Bn",2003);
-    Bike c("Cn",2002);
-    if (test.add_Stock(a))
-    {
-        cout<<"can add bike a into yard"<<endl;
-    }
-    if (test.add_Stock(b))
-    {
-        cout<<"can add bike b into yard"<<endl;
-    }
-    if (test.add_Stock(c))
-    {
-        cout<<"can add bike c into yard"<<endl;
-    }
-    
-    cout<<test.get_Stock_Quantity(2004) << endl;
-    cout<<test.get_Total_Stock_Count() << endl;
-    Bike * ans = test.get_Current_Stock_List();
-    for (int i = 0; i < test.get_Total_Stock_Count(); i++)
-    {
-        cout<<ans[i].getBrand() << " " << ans[i].getCode() << endl;
-    }
-     
+
+
+
 
     return 0;
 }
